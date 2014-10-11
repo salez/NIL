588a9c1f32ed60a75bdeeeeb65b09e1d32787c44
salez <r.sales.s@gmail.com>
2014-10-11 12:14:31 -0300

    battle inicio

---
+++ JogoDoNilson/Controllers/BattleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JogoDoNilson.Controllers
+{
+    public class BattleController : Controller
+    {
+        //
+        // GET: /Battle/
+
+        public ActionResult battle()
+        {
+            return View();
+        }
+
+    }
+}
